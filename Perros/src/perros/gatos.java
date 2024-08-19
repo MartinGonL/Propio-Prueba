824d38220efbbc29b7517da4b378042e707af608
Hector Santiago Subidia <santiagosubidia@gmail.com>
2024-08-19 16:22:46 -0300

    Agregue la clase gatos
    añadi un atributo llamado "patas", con sus constructores, get y set

---
+++ Perros/src/perros/gatos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perros;
+
+/**
+ *
+ * @author santi
+ */
+public class gatos {
+    int patas;
+
+    public gatos() {
+    }
+
+    public gatos(int patas) {
+        this.patas = patas;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+    
+    
+            
+}
